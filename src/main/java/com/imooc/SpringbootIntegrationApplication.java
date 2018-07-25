50a553d1a14f6f4fac9ee31bb2749ff326d56ce2
Roger0917 <1421128167@qq.com>
2018-07-25 22:01:43 +0800

    增加热部署和资源属性配置

---
+++ src/main/java/com/imooc/SpringbootIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIntegrationApplication.class, args);
+	}
+}
---
+++ src/main/java/com/imooc/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.imooc.controller;
+
+import com.imooc.pojo.IMoocJSONResult;
+import com.imooc.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * @Author: Roger
+ * @Date: 2018/7/25 20:27
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/getUser")
+    public Object getUser(){
+        User u = new User();
+        u.setName("imooc");
+        u.setAge(18);
+        u.setBirthday(new Date());
+        u.setPassword("imooc");
+        return u;
+    }
+
+    @RequestMapping("/getUserJson")
+    public Object getUserJson(){
+        User u = new User();
+        u.setName("imooc2");
+        u.setAge(180);
+        u.setBirthday(new Date());
+        u.setPassword("imooc");
+        return IMoocJSONResult.ok(u);
+    }
+}
